aec1e19410f9612baf9687b0b269c6d9eaeb0a07
Gracie Navarro <100546819+avii004@users.noreply.github.com>
2022-06-15 13:44:09 -0500

    Update script.js
    Updated and added to the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,38 @@
 // References for HTML
-// Highscore and timer - links to top of the screen
-let Viewhighscore = document.getElementByID( 'View-high-score' );
-let countdownTimer = document.getElementByID( 'countdown-timer);
-let timeLeft = document.getElementByID( 'times-up');
+// HighScore and Timer - links to top of the screen
+let viewHighScore = document.getElementById('view-high-score');
+let countdownTimer = document.getElementById('countdown-timer');
+let timeLeft = document.getElementById('time-left');
+let timesUp = document.getElementById('times-up');
 
 // Start Section - links to start section and the "Start Quiz" button
-let quizSection = document.getElementByID( 'start-section');
-let startBtn = document.getElementByID( 'startBtn);
+let startSection = document.getElementById('start-section');
+let startBtn = document.getElementById('start-button');
 
-// Quiz section - links to the quiz and buttons
-let quizSection = document.getElementByID( 'quizSection');
-let quizQuestion = document.getElementByID( 'quizQuestion);
+// Quiz Section - links to the quiz and buttons
+let quizSection = document.getElementById('quiz-section');
+let quizQuestion = document.getElementById('quiz-question');
 // Links to button elements
-let choiceA = document.getElementByID( 'button-0' );
-let choiceB = document.getElementByID( 'button-1' );
-let choiceC = document.getElementByID( 'button-2' );
-let choiceD = document.getElementByID( 'button-3' );
+let choiceA = document.getElementById('btn-0');
+let choiceB = document.getElementById('btn-1');
+let choiceC = document.getElementById('btn-2');
+let choiceD = document.getElementById('btn-3');
 
-let answercheck = document.getElementByID( 'answercheck');
+let answerCheck = document.getElementById('answer-check');
 
-// Score Section - links to the final score, player initials input, and submit button
-let scoreSection = document.getElementID( 'scoreSection' );
-let finalScore = document.getElementID( 'finalScore');
-let playerInitials = document.getElementID('playerInitials');
-let playerInitialsBtn = document.getElementID('playerInitialsBtn');
+// Score Section - links to final score, player initials input, and sumbit button
+let scoreSection = document.getElementById('score-section');
+let finalScore = document.getElementById('final-score');
+let playerInitials = document.getElementById('player-initials');
+let playerInitialsBtn = document.getElementById('player-initials-btn');
 
-//High Score Section - links to highscore with a button to go back to start and clesr high score 
-let highScoreSection = document.getElementID('highscore');
-let listOfHighScores = document.getElementID('listOfHighScores');
-let goBackButton = document.getElementID('goBackButton');
-let clearHighScoresButton = document.getElementID('clearHighScoresButton');
+// High Score Section - links to highscore with a button to go back to start and clear highscore
+let highScoreSection = document.getElementById('high-score-section');
+let listOfHighScores = document.getElementById('list-of-high-scores');
+let goBackBtn = document.getElementById('go-back-btn');
+let clearHighScoreBtn = document.getElementById('clear-high-score-btn');
 
-// Set the questions of object
+ // Set the questions of object
 const questions = [
     {
         question: "JavaScript is a ___ -side programming language.",
@@ -99,26 +100,26 @@ const questions = [
 let correctAnswer = 0;
 let questionIndex = 0;
 
-/* Function Section */
-// click "Start Quix" to start timer
-let totaltime=200
+/* FUNCTION SECTION */
+// click "Start Quiz" to start timer
+let totalTime = 150
 function newQuiz() {
     questionIndex = 0;
-    totalTime = 200;
+    totalTime = 150;
     timeLeft.textContent = totalTime;
-    playerInitials.textContent = '';
+    playerInitials.textConent = '';
 
-    startSection.style.display = 'None';
+    startSection.style.display = 'none';
     quizSection.style.display = 'block';
     countdownTimer.style.display = 'block';
-    timesUp.style.display = 'None';
+    timesUp.style.display = 'none';
 
     let startTimer = setInterval(function() {
         totalTime--;
         timeLeft.textContent = totalTime;
-        if (totalTime<=0) {
-            clearInterval (startTimer);
-            if (questionIndex < questions.length - 1){
+        if (totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
                 gameOver();
             }
         }
@@ -128,24 +129,28 @@ function newQuiz() {
 
 // Goes to the next questions of the array
 function nextQuestion() {
-    quizQuestion.textContent = questions [questionIndex].question;
+    quizQuestion.textContent = questions[questionIndex].question;
     choiceA.textContent = questions[questionIndex].choices[0];
     choiceB.textContent = questions[questionIndex].choices[1];
     choiceC.textContent = questions[questionIndex].choices[2];
     choiceD.textContent = questions[questionIndex].choices[3];
 }
 
-//Check if answers are right then updates the correctAnswer to have a point 
+// Checks if the answer is right then updates correctAnswer to have a point
 function checkAnswer(answer) {
-    let lineBreak = document.getElementID( 'lineBreak');
+    let lineBreak = document.getElementById('line-break');
     lineBreak.style.display = 'block';
-    aswerCheck.style.display = 'block';
+    answerCheck.style.display = 'block';
 
-    if (questions[questionIndex].answer === questions[questionIndex].choices[answers]) {
-        correctAnswers++;
-        answerCheck.textContent = totalTime;
-        answerCheck.textContent = `Wrong! The Correct answer is: ${questions[questionsIndex]answers}`
+    if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+        correctAnswer++;
+        answerCheck.textContent = 'Correct!';
+    } else {
+        totalTime -= 10;
+        timeLeft.textContent = totalTime;
+        answerCheck.textContent = `Wrong! The correct answer is: ${questions[questionIndex].answer}`
     }
+
     questionIndex++
     if (questionIndex < questions.length) {
         nextQuestion();
@@ -163,108 +168,108 @@ function chooseB() {
 function chooseC() {
     checkAnswer(2);
 }
-function chooseD() {
+function chooseD() { 
     checkAnswer(3);
 }
 
-// Display the scoreSection, times up and the score of correctAnswer
+// Displays the scoreSection, times up and the score of correctAnswer
 function gameOver() {
     scoreSection.style.display = 'block';
-    quizSection.style.display = 'None';
-    startSection.style.display = 'None';
-    countdownTimer.style.display = 'None';
+    quizSection.style.display = 'none';
+    startSection.style.display = 'none';
+    countdownTimer.style.display = 'none';
     timesUp.style.display = 'block';
-    finalScore.TextContent = correctAnswer;
+    finalScore.textContent = correctAnswer;
 }
 
-// Store players high score in local storage
-function storeHighScore(event) {
+// Stores players high score in local storage
+function storeHighScores(event) {
     event.preventDefault();
     // Prevents player from entering blank value
     if (playerInitials.value === '') {
-        alert('playerInitials');
-        returns;
+        alert('Please enter your initials!');
+        return;
+    }
+
+    startSection.style.display = 'none';
+    countdownTimer.style.display = 'block';
+    timesUp.style.display = 'none';
+    scoreSection.style.display = 'none';
+    highScoreSection.style.display = 'block';
+
+    // pulls from local storage
+    let savedHighScores = localStorage.getItem('high scores');
+    let scoresArray;
+
+    if (savedHighScores === null) {
+        scoresArray = [];
+    } else {
+        scoresArray = JSON.parse(savedHighScores)
+    }
+
+    let userScore = {
+        initials: playerInitials.value,
+        score: finalScore.textContent
+    };
+
+    scoresArray.push(userScore);
+
+    let scoresArrayString = JSON.stringify(scoresArray);
+    window.localStorage.setItem('high scores', scoresArrayString);
+
+    showHighScores();
 }
 
-startSection.style.display = 'None';
-countdownTimer.style.display = 'block';
-timesUp.style.display = 'None';
-scoreSection.Style.display = 'None';
-highSCoreSection.Style.display = 'block';
-
-//Pulls from local storage
-let savedHighSCores = localStorage.getItem( 'highscore' );
-let scoresArray;
-
-if (savedHighSCores ===null) {
-    scoresArray = [];
-} else {
-    scoresArray = Json.parse(savedHighScores)
-}
-
-let userScore = {
-    initials: playerInitials.value,
-    score:finalSCore.textContent
-};
-
-scoresArray.push(userScore);
-
-let scoresArrayString = JSON.stringify(scoresArray);
-window.localStorage.setItem('high scores', scoresArrayString);
-
-showHighScores();
-}
-
-//Showing the high score section and to pull the stored local high scores 
+// To show the High Score Section and to pull the stored local high scores
 function showHighScores() {
     startSection.style.display = 'none';
-    coundownTimer.style.display = 'none';
+    countdownTimer.style.display = 'none';
     quizSection.style.display = 'none';
     timesUp.style.display = 'none';
     scoreSection.style.display = 'none';
     highScoreSection.style.display = 'block';
 
-    let savedHighScores = localStorage.getItem( 'high scores' );
+    let savedHighScores = localStorage.getItem('high scores');
 
-    if (savedHighScores ===null) {
+    if (savedHighScores === null) {
         return;
     }
 
-    let storedHighScore = JSON.parse(savedHighScores);
+    let storedHighScores = JSON.parse(savedHighScores);
 
     for (let i = 0; i < storedHighScores.length; i++) {
-        let eachHighScore = document.createElement( 'p' );
+        let eachHighScore = document.createElement('p');
         eachHighScore.textContent = `${storedHighScores[i].initials}: ${storedHighScores[i].score}`;
-        listOfHighScores.appendCHild(eachHighScore);
+        listOfHighScores.appendChild(eachHighScore);
     }
 }
 
-//Start and 4 choice buttons for the quiz to listen for a click
-startBtn.addEventListener( 'click', newQuiz);
-choiceA.addEventListener( 'click', chooseA);
-choiceB.addEventListener( 'click', chooseB);
-choiceC.addEventListener( 'click', chooseC);
-choiceD.addEventListener( 'click', chooseD);
+// Start and 4 choice buttons for quiz to listen for a click
+startBtn.addEventListener('click', newQuiz);
+choiceA.addEventListener('click', chooseA);
+choiceB.addEventListener('click', chooseB);
+choiceC.addEventListener('click', chooseC);
+choiceD.addEventListener('click', chooseD);
 
-//To store players score and their intials to the local storage
-playerInitialsBtn.addEventListener( 'click', function(event){
-    storedHighScores(event);
+// To store players score and initials to local storage
+playerInitialsBtn.addEventListener('click', function(event){ 
+    storeHighScores(event);
 });
 
-//To show the high score board 
-viewHighScore.addEventListener( 'click', function(event) {
+// To show the High Score Board
+viewHighScore.addEventListener('click', function(event) { 
     showHighScores(event);
 });
 
-//Go back button on the high score section to go back home
-goBackBtn.addEventListener( 'click', function() {
-    startSection.style.display = 'block' ;
+// Go back button on the high score section to go back home
+goBackBtn.addEventListener('click', function() {
+    startSection.style.display = 'block';
     highScoreSection.style.display = 'none';
 });
 
-//Clears high scores in local storage
+// Clears the high scores in local storage 
 clearHighScoreBtn.addEventListener('click', function(){
     window.localStorage.removeItem('high scores');
     listOfHighScores.textContent = 'High Scores Cleared!';
-    listOfHighScores.setAttribute( 'style', 'font-family: 'Courier New', monospace;)
+    listOfHighScores.setAttribute('style', 'font-family: "Poppins", sans-serif; font-style: italic;')
 });
